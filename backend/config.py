9b2bc3a7e2b6092de48ae364b2ee342b11bce59b
Bruno Scorzafave <bruno.scorzafave@aluno.ufabc.edu.br>
2025-11-03 21:11:10 -0300

    feat(backend): add configuration classes for different environments
    Add Config base class with environment variable settings and create DevConfig, ProdConfig, and TestConfig classes for different deployment
    environments. Implement SQLite database configuration for development.

---
+++ backend/config.py
@@ -0,0 +1,20 @@
+from decouple import config
+import os
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+class Config:
+    DEBUG: bool = config('DEBUG', default=False, cast=bool)
+    SECRET_KEY: str = config('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS: bool = config('SQLALCHEMY_TRACK_MODIFICATIONS', default=False, cast=bool)
+
+class DevConfig(Config):
+    DEBUG: bool = True
+    SQLALCHEMY_DATABASE_URI: str = "sqlite:///"+os.path.join(BASE_DIR, 'dev_database.db')
+    SQLALCHEMY_ECHO: bool = True
+
+class ProdConfig(Config):
+    pass
+
+class TestConfig(Config):
+    pass
